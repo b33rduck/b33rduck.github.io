ed6f3d1881bd2eccc9c4b4062382868558eb717e
b33rduck.tom <b33rduck@gmail.com>
2016-10-09 23:46:38 +0200

    Add toggle input

--- src/components/Salutation/Salutation.js
+++ src/components/Salutation/Salutation.js
@@ -1,4 +1,5 @@
 import React, { PropTypes } from 'react';
+import classNames from 'classnames';
 import './salutation.css';
 
 const ENTER_KEY_CODE = 13;
@@ -7,9 +8,11 @@ class Footer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: this.props.userName || '',
-      userName: ''
+      value: this.props.userName || undefined,
+      name: undefined, // todo move this into state
+      hasName: this.props.userName ? true : false,
     };
+    // this.save = this.save.bind(this);
     this.onChange = this.onChange.bind(this);
     this.onKeyDown = this.onKeyDown.bind(this);
   }
@@ -17,7 +20,8 @@ class Footer extends React.Component {
   onKeyDown(e) {
     if (e.keyCode === ENTER_KEY_CODE) {
       this.setState({ 
-        userName: e.target.value
+        name: e.target.value,
+        hasName: !this.state.hasName,
       });
     }
   }
@@ -28,22 +32,38 @@ class Footer extends React.Component {
     });
   }
 
+  // save(e) {
+  //   if (e.target.value.trim()) {
+  //     this.props.onSave(e.target.value);
+  //     this.setState({
+  //       value: '',
+  //     });
+  //   }
+  // }
+
   render() {
     const year = new Date().getFullYear();
-    console.info(this.state.userName);
+    const { hasName } = this.state;
+
+    const classes = classNames({ hasName: hasName });
+    let input;
+    if (hasName) {
+      input = (<h1>Hello {this.state.name}</h1>);
+    }
+
     return (
       <div className="salutation">
-        <h1>What's your Name
-          <input 
+        <h1 className={ classes } >What's your Name
+          <input
             type="text" 
             placeholder={ this.state.placeholder }
-            value= { this.state.value }
             autoFocus={ 1 }
             onBlur={ this.onKeyDown }
             onKeyDown={ this.onKeyDown }
             onChange={ this.onChange }
           />
         </h1>
+        {input}
       </div>
     );
   }
@@ -51,7 +71,8 @@ class Footer extends React.Component {
 
 Footer.propTypes = {
   userName: PropTypes.string,
-  placeholder: PropTypes.string
+  placeholder: PropTypes.string,
+  // onSave: PropTypes.func.isRequired,
 };
 
 export default Footer;
