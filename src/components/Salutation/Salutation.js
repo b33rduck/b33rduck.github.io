b5ca26817052c221163cfe6a528a0962f918f052
b33rduck.tom <b33rduck@gmail.com>
2016-10-09 22:05:31 +0200

    Integrate tests and more dev for Salutation feature

--- src/components/Salutation/Salutation.js
+++ src/components/Salutation/Salutation.js
@@ -1,14 +1,57 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import './salutation.css';
 
-export default class Footer extends React.Component {
+const ENTER_KEY_CODE = 13;
+
+class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: this.props.userName || '',
+      userName: ''
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onKeyDown = this.onKeyDown.bind(this);
+  }
+
+  onKeyDown(e) {
+    if (e.keyCode === ENTER_KEY_CODE) {
+      this.setState({ 
+        userName: e.target.value
+      });
+    }
+  }
+
+  onChange(e) {
+    this.setState({
+      value: e.target.value,
+    });
+  }
 
   render() {
     const year = new Date().getFullYear();
+    console.info(this.state.userName);
     return (
       <div className="salutation">
-        <h1>Your name <input type="text" /></h1>
+        <h1>What's your Name
+          <input 
+            type="text" 
+            placeholder={ this.state.placeholder }
+            value= { this.state.value }
+            autoFocus={ 1 }
+            onBlur={ this.onKeyDown }
+            onKeyDown={ this.onKeyDown }
+            onChange={ this.onChange }
+          />
+        </h1>
       </div>
     );
   }
 }
+
+Footer.propTypes = {
+  userName: PropTypes.string,
+  placeholder: PropTypes.string
+};
+
+export default Footer;
