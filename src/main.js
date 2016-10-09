716d4888d4dfbc1f0c4f7429f31a86e3a516ecd8
b33rduck.tom <b33rduck@gmail.com>
2016-10-09 17:29:38 +0200

    Replace rollup with webpack again

--- src/main.js
+++ src/main.js
@@ -6,15 +6,6 @@ import App from './components/App';
 import './styles/normalize.css';
 import './styles/main.css';
 
-if (ENV !== 'production') {
-  // Enable LiveReload
-  document.write(
-    '<script src="http://' + (location.host || 'localhost').split(':')[0] +
-    ':35729/livereload.js?snipver=1"></' + 'script>'
-  );
-}
-
-
 const currentdate = new Date();
 console.info(`${currentdate.toLocaleTimeString()} \\\\ You can't stop here, this is b33rduck land.`);
 
