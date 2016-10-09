73bfc000fe6a87d0dba1da74fb424dd34b81381d
b33rduck.tom <b33rduck@gmail.com>
2016-10-09 17:58:01 +0200

    Use prod webpack config for master

---
+++ webpack.config.prod.js
@@ -0,0 +1,52 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './src/main'
+  ],
+  output: {
+    path: 'public',
+    filename: 'main.js',
+    publicPath: './public/'
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
+  module: {
+    preLoaders: [
+      {
+        test: /\.js/,
+        loader: 'eslint'
+      }
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.css$/,
+        loader: "style!css"
+      },
+      {
+        test: /\.(eot|woff|woff2|ttf)$/,
+        loader: 'url-loader?limit=30000&name=[name].[ext]'
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'file-loader?name=images/[name].[ext]'
+      }
+    ]
+  }
+};
